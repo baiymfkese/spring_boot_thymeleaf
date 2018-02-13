6ba753d19589669792ce057ce7a787228969ae2e
guoming <guoming3@jd.com>
2018-02-13 10:46:02 +0800

    初始化项目结构

---
+++ spring_boot_service/src/main/java/com/example/service/student/StudentService.java
@@ -0,0 +1,13 @@
+package com.example.service.student;
+
+/**
+ * Created by guoming3 on 2018/2/13.
+ */
+public interface StudentService {
+
+    /**
+     * 获取学生名字
+     * @return
+     */
+    String getStudentName();
+}
---
+++ spring_boot_service/src/main/java/com/example/service/student/impl/StudentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.service.student.impl;
+
+import com.example.service.student.StudentService;
+
+/**
+ * Created by guoming3 on 2018/2/13.
+ */
+public class StudentServiceImpl implements StudentService {
+
+    @Override
+    public String getStudentName() {
+        return "lisi";
+    }
+}
